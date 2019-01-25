88344fb1bc1710c53d738c27e7e87e9e6be269e6
Michelle Gaffney <mgaff87@gmail.com>
2019-01-25 15:38:06 -0500

    refactored using redux

--- src/components/top-nav.js
+++ src/components/top-nav.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
+import {generateAuralUpdate, restartGame} from '../actions';
+
 import './top-nav.css';
 
 export function TopNav(props) {
@@ -21,7 +23,7 @@ export function TopNav(props) {
             href="#feedback"
             className="new"
             aria-label="Start a new game"
-            onClick={() => props.onRestartGame()}
+            onClick={() => props.dispatch(restartGame(Math.floor(Math.random() * 100) + 1))}
           >
             + New Game
           </a>
@@ -32,7 +34,7 @@ export function TopNav(props) {
             /* the `visuallyhidden` class hides an element 
             while leaving it available to screen reader users  */
             className="visuallyhidden focusable status-link"
-            onClick={() => props.onGenerateAuralUpdate()}
+            onClick={() => props.dispatch(generateAuralUpdate())}
           >
             Hear state of game
           </a>
--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import {createStore} from 'redux';
 
-import {hotColdReducer} from './reducers';
+import reducer from './reducer';
 
-export default createStore(hotColdReducer);
+export default createStore(reducer);
